705a71162f3cebcf384ab64f959069d610535643
ahmadihsanullah <ahma21327ti@student.nurulfikri.ac.id>
2023-10-09 22:43:22 +0700

    penerapan layout: linear layout, table layout,relative layout, frame layout

---
+++ app/src/main/java/infinite/learning/belajarandroiddasar/layout/MovieActivity.kt
@@ -0,0 +1,12 @@
+package infinite.learning.belajarandroiddasar.layout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import infinite.learning.belajarandroiddasar.R
+
+class MovieActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movie)
+    }
+}
